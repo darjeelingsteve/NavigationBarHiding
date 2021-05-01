10e2a4c40b4fd4645f1e3cee0b198446ac6af3d8
Stephen Anthony <steve@darjeelingapps.com>
2021-05-01 14:38:12 +0100

    Show UISplitViewController in main scene window

--- NavigationBarHiding/SceneDelegate.swift
+++ NavigationBarHiding/SceneDelegate.swift
@@ -13,6 +13,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
+        let splitViewController = UISplitViewController(style: .doubleColumn)
+        splitViewController.preferredDisplayMode = .oneBesideSecondary
+        splitViewController.preferredSplitBehavior = .tile
+        window?.rootViewController = splitViewController
         window?.makeKeyAndVisible()
     }
 }
